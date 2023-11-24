8d4d9f8a26d86a324f552659a923f9382642badd
touhidcodes <touhidcodes@gmail.com>
2023-11-24 17:41:53 +0600

    user services aed

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,22 +1,41 @@
 import { User } from "./user.interface";
 import UserModel from "./user.model";
 
-const createUserIntoDB = async (user: User) => {
+const createUserIntoDB = async (user: User): Promise<User> => {
   const result = await UserModel.create(user);
   return result;
 };
 
-const getUserFromDB = async () => {
+const getUserFromDB = async (): Promise<User[]> => {
   const result = await UserModel.find();
   return result;
 };
 
-const getSingleUserFromDB = async (id: string) => {
+const getSingleUserFromDB = async (id: string): Promise<User | null> => {
   const result = await UserModel.findOne({ userId: id });
   return result;
 };
+
+const updateUserIntoDB = async (
+  id: string,
+  userData: User
+): Promise<User | null> => {
+  const result = await UserModel.findByIdAndUpdate(id, userData, {
+    new: true,
+    runValidators: true,
+  });
+  return result;
+};
+
+const deleteUserFromDB = async (id: string): Promise<User | null> => {
+  const result = await UserModel.findByIdAndDelete(id);
+  return result;
+};
+
 export const UserServices = {
   createUserIntoDB,
   getUserFromDB,
   getSingleUserFromDB,
+  updateUserIntoDB,
+  deleteUserFromDB,
 };
