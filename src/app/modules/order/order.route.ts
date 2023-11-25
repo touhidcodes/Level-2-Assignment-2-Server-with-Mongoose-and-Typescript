98c4e69d2139d22b331ba8301d5db33eb1da5fe7
touhidcodes <touhidcodes@gmail.com>
2023-11-25 12:23:46 +0600

    order module removed

--- src/app/modules/order/order.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { OrderControllers } from "./order.controller";
-
-const router = express.Router();
-
-router.post("/:userId/orders", OrderControllers.createOrder);
-
-export const OrderRoutes = router;
